99b9c19157fed587b018ac2a940162a66f6052d0
Pranav Trivedi <pranavtrivedi1993@gmail.com>
2021-08-07 11:12:51 +0530

    test cases for archive unarchive api

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -9,11 +9,51 @@ RSpec.describe 'Users', type: :request do
     )
   end
 
+  let(:another_user) do
+    User.create(
+      email: 'john@example.com',
+      password: 'securepassword',
+      password_confirmation: 'securepassword',
+    )
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
       auth_token = authenticate_user(user)
       get users_path, headers: { 'Authentication' => "Bearer #{auth_token}" }
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST /archive' do
+    it 'returns http ok' do
+      auth_token = authenticate_user(user)
+      post archive_user_path(another_user), headers: { 'Authentication' => "Bearer #{auth_token}" }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST /unarchive' do
+    it 'returns http ok' do
+      auth_token = authenticate_user(user)
+      post unarchive_user_path(another_user), headers: { 'Authentication' => "Bearer #{auth_token}" }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST /archive' do
+    it 'returns http unprocessable_entity' do
+      auth_token = authenticate_user(user)
+      post archive_user_path(user), headers: { 'Authentication' => "Bearer #{auth_token}" }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'POST /unarchive' do
+    it 'returns http unprocessable_entity' do
+      auth_token = authenticate_user(user)
+      post unarchive_user_path(user), headers: { 'Authentication' => "Bearer #{auth_token}" }
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
